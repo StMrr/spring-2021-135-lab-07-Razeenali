6e0e2a1230580f30696a02d77fb43bae376861bf
StMrr <boldeagle175@gmail.com>
2021-04-22 11:14:12 -0400

    added comments

--- main.cpp
+++ main.cpp
@@ -16,16 +16,17 @@ int main()
     cout << removeLeadingSpaces(line) << endl;
     mcount = mcount -  countChar(removeLeadingSpaces(line),'}');
     indented = getTab(mcount)+ removeLeadingSpaces(line);
-    if(restart){
+    if(restart){ // gets rid of the extra tab now that it isn't needed for the other lines.
       mcount--;
       restart = false;
     }
     task_b.push_back(indented);
     mcount = mcount + countChar(removeLeadingSpaces(line), '{');
     if(countChar(removeLeadingSpaces(line), '(')+countChar(removeLeadingSpaces(line), ')') == 2){
-      if(countChar(removeLeadingSpaces(line), '{') == 0){
+      if(countChar(removeLeadingSpaces(line), '{') == 0){ //loops without brackets still have two parenthises
+        //no matter what, allows me to check for a line with a pair of them but no bracket afterward
         mcount++;
-        restart = true;
+        restart = true; //set as a way to trigger getting rid of the extra tab that gets added
       }
     }
   }
